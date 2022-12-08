44417907552f5be0799cd47dcfd60f203ba03bdf
Jubel122 <117885164+Jubel122@users.noreply.github.com>
2022-12-08 12:50:01 +0700

    Add files via upload

---
+++ 1_A_71220945.py
@@ -0,0 +1,7 @@
+angka= int(input("Masukkan input"))
+print(''(angka-1),"")
+for angka2 in range((angka-1),0,1):
+    print (''(angka2-1),"*")
+    print ("*"*angka)   
+
+
---
+++ 1_B_71220945.py
@@ -0,0 +1,13 @@
+print("selamat datang di kalkulator sedeherhana")
+
+print('=' * 25)
+print('kalkulator sederhana')
+print('  1. Jumlah \t [+]')
+print('  2. Kurang \t [-]')
+print('  3. Kali \t [*]')
+print('  4. Bagi \t [/]')
+print('=' * 25)
+
+operasi = input('Pilih operasi (1/2/3/4): ')
+bilangan_1 = int(input('Masukkan bilangan pertama: '))
+bilangan_2 = int(input('Masukkan bilangan kedua: '))
